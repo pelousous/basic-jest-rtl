54874a1d3963afceb4bce1b783d0d1f895693cc8
davide <info@davideravasi.com>
2022-06-08 09:44:44 +0000

    add test to check if Confirm order button is disabled

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,5 +1,6 @@
 // CUSTOM RENDER https://testing-library.com/docs/react-testing-library/setup
 import { render, screen, waitFor } from "../../../test-utils/test-utils";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { server } from "../../../mocks/server";
 import { rest } from "msw";
@@ -24,4 +25,35 @@ test("handling error on scoop options and toppings options", async () => {
 
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("check if button cnfirm order is disabled when no scoops added", async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />, {
+    wrapper: OrderDetailsProvider,
+  });
+
+  const scoopsTotal = await screen.findByText("Scoops total: $", {
+    exact: false,
+  });
+
+  const confirmOrder = await screen.findByRole("button", {
+    name: /Confirm order/i,
+  });
+
+  expect(scoopsTotal).toHaveTextContent("0.00");
+  expect(confirmOrder).toBeDisabled();
+
+  const vanillaOption = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  await userEvent.clear(vanillaOption);
+  await userEvent.type(vanillaOption, "1");
+
+  expect(confirmOrder).toBeEnabled();
+
+  await userEvent.clear(vanillaOption);
+  await userEvent.type(vanillaOption, "0");
+
+  expect(confirmOrder).toBeDisabled();
 });
