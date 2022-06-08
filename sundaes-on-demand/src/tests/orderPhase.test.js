8c26285cdb529c8c610a822af01b484986b544d1
davide <info@davideravasi.com>
2022-06-08 08:44:38 +0000

    added tests for loader and no toppings on summary

--- sundaes-on-demand/src/tests/orderPhase.test.js
+++ sundaes-on-demand/src/tests/orderPhase.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from '../App';
@@ -37,18 +37,33 @@ test('order Phase for happy path', async () => {
 
   await userEvent.click(checkboxTerms);
 
-  const submitOrder = await screen.getByRole('button', { name: 'Confirm order' });
+  const submitOrder = screen.getByRole('button', { name: 'Confirm order' });
 
   await userEvent.click(submitOrder);
+
+  // check if loading is there
+  const loading = screen.getByText('Loading');
+  expect(loading).toBeInTheDocument();
+
+  // check confirmation page text
+  const thankYou = await screen.findByRole('heading', {name: 'Thank You!'});
+
+  expect(thankYou).toBeInTheDocument();
+
+  // check LOADER disappear when confirmation page is loaded
+  // await waitForElementToBeRemoved(() => screen.queryByText('Loading'));
+
+  await waitFor(() => {
+    expect(screen.queryByText('Loading')).not.toBeInTheDocument();
+  });
 
   // confirm order number on confirmation page
   const orderNmbrPar = await screen.findByText('Your order number is ', { exact: false });
 
   await expect(orderNmbrPar).toHaveTextContent('7392822562');
 
-
   // click "new order" button on confirmation page
-  const newOrderBtn = await screen.getByRole('button', { name: 'Create new order' });
+  const newOrderBtn = screen.getByRole('button', { name: 'Create new order' });
 
   await userEvent.click(newOrderBtn);
 
@@ -62,4 +77,26 @@ test('order Phase for happy path', async () => {
   // check if the buttons are presents and the call is succesfull
   await screen.findByRole('spinbutton', { name: 'Vanilla' });
   await screen.findByRole('checkbox', { name: 'Cherries' });
+});
+
+test('order Phase for happy path with NO TOPPINGS', async () => {
+  render(<App />);
+
+  const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+  const vanillaOption = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+
+  await userEvent.clear(vanillaOption);
+  await userEvent.type(vanillaOption, '1');
+
+  expect(grandTotal).toHaveTextContent('2.00');
+
+  const confirmOrder = await screen.findByText('Confirm order', { exact: false });
+
+  await userEvent.click(confirmOrder);
+
+  const toppingstotal = screen.queryByText('Toppings: ', { exact: false });
+
+  expect(toppingstotal).not.toBeInTheDocument();
+
 });
